3bd1ba06bdd721d9802ebbe106c714ec3dfd8fef
Olinto <olintojose@gmail.com>
2020-12-09 06:07:49 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '02a724db3ca7b0700a09836ddac29289'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6b5da883-731b-4c7c-b9df-065606de7307'
+};
